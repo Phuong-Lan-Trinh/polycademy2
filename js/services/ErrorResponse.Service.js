c3adfcbadbb9d45c3e865f5f393202e6b06f03a7
Roger Qiu <roger.qiu@polycademy.com>
2013-03-19 05:31:50 +1100

    Added comments of refactor plan

--- js/services/ErrorResponse.Service.js
+++ js/services/ErrorResponse.Service.js
@@ -1,5 +1,15 @@
 'use strict';
 
+//REFACTOR PLAN
+
+//1. Setup a model variable outside the httpProvider and compileProvider
+//2. Inside the interceptor, push error messages to the model variable
+//3. Inside directive, take the model variable, and bind it to the scope.
+//4. Inside the HTML, take the model and do an ng-repeat (apply ng-show) on that model too!
+//5. Messages can be acquired from the response itself! Use error variable... (Actually don't use that, use your own, decouple the client from the server in this case!)
+//6. Change the HTML so it's applied like an alert box, but one which appears and fades away. Up on top of the UI... (overlayed on top), AJAX loading always happens first, and then error box message
+//7. Remember some messages will be reshown as part of validation...? So these are the temporary messages!
+
 /**
  * Response Handler for Error Codes across all HTTP requests to show an alert box!
  */
